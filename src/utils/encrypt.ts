89d916a463a053e0223e8918a31c8c80afb3b750
14 <aptx.wang@gmail.com>
2023-04-17 17:13:01 +0800

    feat: use aesjs instead

--- src/utils/encrypt.ts
+++ src/utils/encrypt.ts
@@ -1,11 +1,15 @@
-import CryptoJS from 'crypto-js'
+import aesjs from 'aes-js'
 
-export function encrypt(key, text) {
-  const encrypted = CryptoJS.AES.encrypt(text, key)
-  return encrypted.toString()
+export function encrypt(key: string, text: string) {
+  // eslint-disable-next-line new-cap
+  const aesCtr = new aesjs.ModeOfOperation.ctr(aesjs.utils.hex.toBytes(key))
+  const encryptedBytes = aesCtr.encrypt(aesjs.utils.utf8.toBytes(text))
+  return aesjs.utils.hex.fromBytes(encryptedBytes)
 }
 
-export function decrypt(key, text) {
-  const decrypted = CryptoJS.AES.decrypt(text, key)
-  return decrypted.toString(CryptoJS.enc.Utf8)
+export function decrypt(key: string, text: string) {
+  // eslint-disable-next-line new-cap
+  const aesCtr = new aesjs.ModeOfOperation.ctr(aesjs.utils.hex.toBytes(key))
+  const decryptedBytes = aesCtr.decrypt(aesjs.utils.hex.toBytes(text))
+  return aesjs.utils.utf8.fromBytes(decryptedBytes)
 }
