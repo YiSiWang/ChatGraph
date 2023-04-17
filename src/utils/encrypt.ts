af9f7e606e3cc1ea292c2134fd6aa94b6678808d
14 <aptx.wang@gmail.com>
2023-04-17 15:51:09 +0800

    test: test remove crypto-js

--- src/utils/encrypt.ts
+++ src/utils/encrypt.ts
@@ -1,11 +1,7 @@
-import CryptoJS from 'crypto-js'
-
 export function encrypt(key, text) {
-  const encrypted = CryptoJS.AES.encrypt(text, key)
-  return encrypted.toString()
+  return key + text
 }
 
 export function decrypt(key, text) {
-  const decrypted = CryptoJS.AES.decrypt(text, key)
-  return decrypted.toString(CryptoJS.enc.Utf8)
+  return key + text
 }
