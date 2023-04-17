87eed93fb8ec7274f5106ba10ed26d44dd8449f4
14 <aptx.wang@gmail.com>
2023-04-17 15:18:07 +0800

    feat: update encrypt method

--- src/utils/encrypt.ts
+++ src/utils/encrypt.ts
@@ -1,26 +1,11 @@
-import crypto from 'crypto'
-
-const ALGORITHM = 'aes-256-cbc'
-const IV_LENGTH = 16
+import CryptoJS from 'crypto-js'
 
 export function encrypt(key, text) {
-  const iv = crypto.randomBytes(IV_LENGTH)
-  const cipher = crypto.createCipheriv(ALGORITHM, Buffer.from(key, 'hex'), iv)
-  let encrypted = cipher.update(text)
-  encrypted = Buffer.concat([encrypted, cipher.final()])
-  return `${iv.toString('hex')}:${encrypted.toString('hex')}`
+  const encrypted = CryptoJS.AES.encrypt(text, key)
+  return encrypted.toString()
 }
 
-export function decrypt(key: string, text: string) {
-  const textParts = text.split(':')
-  const iv = Buffer.from(textParts.shift(), 'hex')
-  const encryptedText = Buffer.from(textParts.join(':'), 'hex')
-  const decipher = crypto.createDecipheriv(
-    ALGORITHM,
-    Buffer.from(key, 'hex'),
-    iv,
-  )
-  let decrypted = decipher.update(encryptedText)
-  decrypted = Buffer.concat([decrypted, decipher.final()])
-  return decrypted.toString()
+export function decrypt(key, text) {
+  const decrypted = CryptoJS.AES.decrypt(text, key)
+  return decrypted.toString(CryptoJS.enc.Utf8)
 }
