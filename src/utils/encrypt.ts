8d78087c711c714ef42a3a4022bd2b3cb67c92f8
14 <aptx.wang@gmail.com>
2023-04-17 16:12:56 +0800

    test: use crypto-js/aes

--- src/utils/encrypt.ts
+++ src/utils/encrypt.ts
@@ -1,7 +1,12 @@
+import AES from 'crypto-js/aes'
+import Utf8 from 'crypto-js/enc-utf8'
+
 export function encrypt(key, text) {
-  return key + text
+  const encrypted = AES.encrypt(text, key)
+  return encrypted.toString()
 }
 
 export function decrypt(key, text) {
-  return key + text
+  const decrypted = AES.decrypt(text, key)
+  return decrypted.toString(Utf8)
 }
